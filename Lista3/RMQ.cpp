22f92de773fd181cc02dfd52cd4dd99bc0a852b2
Tiago Moraes <tbm2@cin.ufpe.br>
2018-09-26 07:05:25 -0300

    Add the code for the father pointer and for the array with leaves

--- Lista3/RMQ.cpp
+++ Lista3/RMQ.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 struct segment_tree {
-    int l, r, m;
-    segment_tree *left, *right;
+    int l, r, m, numLeaves;
+    segment_tree *left, *right, *father;
 
     segment_tree(int arr[], int l, int r) {
         this->l = l;
@@ -20,17 +20,23 @@ struct segment_tree {
 
             this->left = new segment_tree(arr, l, h);
             this->right = new segment_tree(arr, h+1, r);
+            this->left->father = this;
+            this->right->father = this;
             this->m = min(this->left->m, this->right->m);
         }
     }
 
+    /*
     void destroyRecursive() {
         if(this) {
             this->left->destroyRecursive();
             this->right->destroyRecursive();
             delete this;
+        } else {
+            delete this;
         }
     }
+    */
 
     int RMQ(int a, int b) {
         if(a > b || a > this->r || b < this->l) {
@@ -45,23 +51,46 @@ struct segment_tree {
 
     void print(int a, int b) {
         // percorrer a árvore toda em pós ordem e printar apenas os valores que tiverem algu valor do meu intervalo
-        if(a > b || a > this->r || b < this->l) {
-            return;
-        } else if(a == this->l && b == this->r) {
-            cout << this->m << " ";
-            return;
-        } else {
-            int h = floor((this->l+this->r)/2);
-            this->left->print(max(a,this->l), min(b,h));
-            this->right->print(max(a,h+1), min(b,this->r));
+        if(this == nullptr) {
             return;
         }
+
+        if(this->left != nullptr) {
+            this->left->print(a, b);
+        }
+
+        if(this->right != nullptr) {
+            this->right->print(a, b);
+        }
+
+        if(a <= this->r && b >= this->l) {
+            if(this->father == nullptr) {
+                cout << this->m;
+            } else {
+                cout << this->m << " ";
+            }
+        }
+
     }
-
-
 };
 
 typedef struct segment_tree* stPtr;
+
+vector<stPtr> leaves;
+
+void fillLeaves(stPtr st) {
+    if(st->left == nullptr && st->right == nullptr) {
+        ::leaves.push_back(st);
+    }
+
+    if(st->left != nullptr) {
+        fillLeaves(st->left);
+    }
+
+    if(st->right != nullptr) {
+        fillLeaves(st->right);
+    }
+}
 
 int main(int argc, char *argv[]) {
     ios::sync_with_stdio(false);
@@ -73,7 +102,9 @@ int main(int argc, char *argv[]) {
     int sensores[numSensores];
 
     for(int i = 0; i < numSensores; i++) {
-        cin >> sensores[i];
+        int aux;
+        cin >> aux;
+        sensores[i] = aux;
     }
 
     stPtr st = new segment_tree(sensores, 0, numSensores-1);
@@ -89,6 +120,7 @@ int main(int argc, char *argv[]) {
             cin >> i;
             cin >> x;
             sensores[i] = x;
+            stPtr aux = st;
             st = new segment_tree(sensores, 0, numSensores-1);
         } else if(command == "RMQ") {
             int l, r;
@@ -105,6 +137,13 @@ int main(int argc, char *argv[]) {
             end = true;
         }
     }
+
+    fillLeaves(st);
+
+    for(int i = 0; i < numSensores; i++) {
+        cout << ::leaves[i]->father->l << "|" << ::leaves[i]->father->r << endl;
+    }
+
 
     return 0;
 }
